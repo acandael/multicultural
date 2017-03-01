623bb7add106527f57be73e74f39d1d9e6ca3110
acandael <anthony.candaele@ugent.be>
2017-03-01 16:03:48 +0100

    implement searchengine sitemap

--- config.rb
+++ config.rb
@@ -37,8 +37,11 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 end
+
+set :url_root, 'http://www.proceduralfairness.ugent.be'
+activate :search_engine_sitemap
